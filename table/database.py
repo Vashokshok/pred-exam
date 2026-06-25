f7319550c4c404ad0b3d8deb5947b9942a1795fb
Vashokshok <vashokshok@mail.ru>
2026-06-25 18:12:02 +0300

    Add project

---
+++ table/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import  Base
+
+engine = create_engine('sqlite:///datbase.db', echo=True)
+
+Session = sessionmaker(bind=engine)
+
---
+++ table/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, Integer, Column, ForeignKey, Float
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class CarDealer(Base):
+    __tablename__ = 'cardealers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    city = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+
+    car_id = relationship('Car', back_populates='carr')
+
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    id = Column(Integer, primary_key=True)
+    brand = Column(String, nullable=False)
+    model = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    price = Column(Float, nullable=True)
+    dealer_id = Column(Integer, ForeignKey('cardealers.id', ondelete='CASCADE'), nullable=False)
+
+    carr = relationship('CarDealer', back_populates='car_id')
