05b828268afaae0d07a7c21678c52f884c089965
Vashokshok <vashokshok@mail.ru>
2026-06-30 14:39:43 +0300

    Add routes and html

--- app.py
+++ app.py
@@ -2,22 +2,18 @@ from flask import Flask, render_template
 from flask_login import current_user
 from table.database import session
 from table.models import CarDealer, Car
+from auto.routes import car_bp
+
 
 app = Flask(__name__)
 
+app.register_blueprint(car_bp, url_prefix='/cars')
+
 
 @app.route('/')
-def mainO():
+def main():
     dealers = session.query(CarDealer).all()
     return render_template('index.html', current_user=current_user, dealers=dealers)
-
-
-@app.route('/add_dealer')
-def add_dealer():
-        dealer = CarDealer(name='АвтоСалон Грозный', city='Грозный', address='ул. Ленина, 5')
-        session.add(dealer)
-        session.commit()
-        return f'Добавлен диллер: {dealer.name}'
 
 
 if __name__ == '__main__':
---
+++ auto/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request, redirect, url_for, render_template
+from table.models import CarDealer
+from table.database import session
+
+car_bp = Blueprint('cars', __name__, template_folder='templates')
+
+@car_bp.route('/', methods=['GET', 'POST'])
+def add_auto():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        city = request.form.get('city')
+        address = request.form.get('address')
+        dealer = CarDealer(name=name, city=city, address=address)
+        session.add(dealer)
+        session.commit()
+        return redirect(url_for('main'))
+    return render_template('add_dealers.html')
